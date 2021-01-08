f0528846230e122732ef1b1d360a9f735b3bc05b
lojithv <lojithvinsuka1998@gmail.com>
2021-01-08 08:11:58 +0530

    basic layout updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import 'jquery/dist/jquery.min.js';
 import 'popper.js/dist/umd/popper.min.js'
-// import $ from 'jquery';
+import $ from 'jquery';
 
 class App extends React.Component {
   componentDidMount(){
@@ -22,51 +22,72 @@ class App extends React.Component {
             <div class="bg-light border-right" id="sidebar-wrapper">
               <div class="sidebar-heading">Therichpost </div>
               <div class="list-group list-group-flush">
-                <a href="#" class="list-group-item list-group-item-action bg-light">Dashboard</a>
-                <a href="#" class="list-group-item list-group-item-action bg-light">Shortcuts</a>
-                <a href="#" class="list-group-item list-group-item-action bg-light">Overview</a>
-                <a href="#" class="list-group-item list-group-item-action bg-light">Events</a>
-                <a href="#" class="list-group-item list-group-item-action bg-light">Profile</a>
+                <a href="#" class="list-group-item list-group-item-action bg-light">HOME</a>
+                <a href="#" class="list-group-item list-group-item-action bg-light">FAVS</a>
+                <a href="#" class="list-group-item list-group-item-action bg-light">POPULAR</a>
+                <a href="#" class="list-group-item list-group-item-action bg-light">NEW</a>
+                <a href="#" class="list-group-item list-group-item-action bg-light">CATEGORIES</a>
                 <a href="#" class="list-group-item list-group-item-action bg-light">Status</a>
               </div>
             </div>
+
             <div id="page-content-wrapper">
               <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
                 <button class="btn btn-primary" id="menu-toggle">Toggle Menu</button>
+                <button class="btn btn-primary  ml-2" id="logo-link">LOGO</button>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                   <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                   <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
                     <li class="nav-item active">
-                      <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                      <a class="nav-link" href="#">NOTIFICATIONS <span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item">
-                      <a class="nav-link" href="#">Link</a>
+                      <a class="nav-link" href="#">MESSAGES</a>
                     </li>
                     <li class="nav-item dropdown">
                       <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Dropdown
+                        PROFILE ICON
                       </a>
                       <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="#">Action</a>
-                        <a class="dropdown-item" href="#">Another action</a>
+                        <a class="dropdown-item" href="#">PROFILE NAME</a>
+                        <a class="dropdown-item" href="#">SELLING</a>
+                        <a class="dropdown-item" href="#">BUYING</a>
+                        <a class="dropdown-item" href="#">WATCHLIST</a>
+                        <a class="dropdown-item" href="#">BOOKMARKS</a>
                         <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#">Something else here</a>
+                        <a class="dropdown-item" href="#">SETTINGS</a>
+                        <a class="dropdown-item" href="#">HELP CENTER</a>
+                        <a class="dropdown-item" href="#">LOG OUT</a>
                       </div>
                     </li>
                   </ul>
                 </div>
               </nav>
+
+              <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+                  <li class="nav-item active">
+                      <a class="nav-link" href="#">CREATIONS <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item active">
+                      <a class="nav-link" href="#">FEED <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item active">
+                      <a class="nav-link" href="#">JOBS <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item active">
+                      <a class="nav-link" href="#">MARKETPLACE <span class="sr-only">(current)</span></a>
+                    </li>
+                  </ul>
+                </div>
+              </nav>
+
               <div class="container-fluid">
-                <h1 class="mt-4">Reactjs Sidebar Responsive Template - Therichpost.com</h1>
-                <p>Reactjs Sidebar Responsive Template - Therichpost.com 
-                    Reactjs Sidebar Responsive Template - Therichpost.com
-                    Reactjs Sidebar Responsive Template - Therichpost.com
-                    Reactjs Sidebar Responsive Template - Therichpost.com
-                    Reactjs Sidebar Responsive Template - Therichpost.com
-                    Reactjs Sidebar Responsive Template - Therichpost.com
-                    Reactjs Sidebar Responsive Template - Therichpost.com
+                <h1 class="mt-4">HOME</h1>
+                <p>POSTS WILL APPEAR HERE
                 </p>
                 
               </div>
